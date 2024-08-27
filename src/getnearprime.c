a6e79c1341ee9715ffbb91fdc1cb8c922291b8f2
Pablo Escobar <sataniv.rider@gmail.com>
2024-08-27 16:53:36 +0200

    correct find prime numbers

--- src/getnearprime.c
+++ src/getnearprime.c
@@ -6,7 +6,7 @@
 /*   By: Pablo Escobar <sataniv.rider@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 20:26:40 by Pablo Escob       #+#    #+#             */
-/*   Updated: 2024/08/24 21:24:56 by Pablo Escob      ###   ########.fr       */
+/*   Updated: 2024/08/27 16:52:55 by Pablo Escob      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ static t_uchar	*initprimearray(unsigned long sizearr)
 	int		i;
 	t_uchar	*tmp;
 
-	sizearr = sizearr / FT_BIT_MAX + 1;
+	sizearr = sizearr / FT_BIT_MAX + 2;
 	tmp = malloc(sizearr * sizeof(t_uchar));
 	if (!tmp)
 	{
@@ -59,6 +59,8 @@ t_ulong	getmin(t_ulong prime, t_ulong minnum)
 {
 	if (minnum == prime)
 		minnum *= prime;
+	if (minnum < prime)
+		minnum = prime + 1;
 	while (minnum % prime)
 		++minnum;
 	return (minnum);
@@ -66,27 +68,38 @@ t_ulong	getmin(t_ulong prime, t_ulong minnum)
 
 static void	setnprm(t_uchar *primearr, t_ulong maxnum, t_ulong minnum, t_ulong prm)
 {
+	t_uint	byte;
+	t_uint	bit;
 	t_uint	i;
 
-	i = minnum;
+	if (minnum < 2)
+		i = getmin(prm, prm);
+	else
+		i = getmin(prm, minnum);
 	while (i <= maxnum)
 	{
-		resetbit(&(primearr[(i - minnum) / FT_BIT_MAX]), (i - minnum) % FT_BIT_MAX);
+		bit = (i - minnum) % FT_BIT_MAX;
+		byte = (i - minnum) / FT_BIT_MAX;
+		resetbit(&(primearr[byte]),
+			bit);
 		i += prm;
 	}		
 }
 
 t_uchar	*setnonprime(t_llist *primenums, t_ulong maxnum, t_ulong minnum)
 {
-	t_ulong	tmp;
 	t_uchar *primearr;
 
 	primearr = initprimearray(maxnum - minnum + 2);
-	while (primenums)
+	if (minnum < 2)
 	{
-		tmp = getmin(*(int *)primenums->data, minnum);
+		resetbit(&(*primearr), 0);
+		resetbit(&(*primearr), 1);
+	}
+	while (primenums && *(int *)primenums->data <= maxnum)
+	{
 		setnprm(primearr, maxnum,
-			tmp, *(int *)primenums->data);
+			minnum, *(int *)primenums->data);
 		primenums = primenums->next;
 	}
 	return (primearr);
@@ -157,16 +170,37 @@ t_llist	*ft_getllistprimes(t_uchar *markedprimes, t_ulong maxnum, t_ulong minnum
 	return (primenums);
 }
 
+void	printmarkbits(t_uchar *markbits, t_ulong minnum)
+{
+	int	i;
+	int	bit;
+
+	i = -1;
+	while (markbits[++i] != FT_UCHARMAXVAL)
+	{
+		bit = 0;
+		while (bit < FT_BIT_MAX)
+		{
+			if (getbit(&markbits[i], bit))
+				printf ("TEST: PRIME NUMBER:\t%ld\n", i * FT_BIT_MAX + minnum + bit);
+			++bit;
+		}
+	}
+}
+
 int main()
 {
-	t_ulong	maxnum = 113;
-	t_ulong	minnum = 101;
+	t_ulong	maxnum = 1001;
+	t_ulong	minnum = 900;
 	t_uchar	*primearr;
 	t_llist	*elementprimes;
 	t_llist	*primenum;
 
-	elementprimes = ft_getprimes((t_ulong)ft_sqrt(maxnum));
+	elementprimes = ft_getprimes((t_ulong)(ft_sqrt(maxnum)));
+	// llistiter(elementprimes, printdata);
+	// printmarkbits(elementprimes, 0);
 	primearr = setnonprime(elementprimes, maxnum, minnum);
+	// printmarkbits(primearr, minnum);
 	primenum = ft_getllistprimes(primearr, maxnum, minnum);
 	llistiter(primenum, printdata);
 	return (0);
